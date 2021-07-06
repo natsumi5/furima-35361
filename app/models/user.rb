af74ff6f69cd12729058e3308256479f38494031
natsumi5 <ntm2038@gmail.com>
2021-07-06 15:37:29 +0900

    ユーザーモデル単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :encrypted_password, :password,:password_confirmation, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: '英字と数字の両方を含めて設定してください。'}
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナ（全角）カタカナで入力して下さい。'}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナ（全角）で入力して下さい。'}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '漢字・ひらがな・カタカナ（全角）で入力して下さい。'}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナ（全角）で入力して下さい。'}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナ（全角）で入力して下さい。'}
