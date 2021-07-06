86b019a5d52babaa66c3eefac339443dd8ec18a5
natsumi5 <ntm2038@gmail.com>
2021-07-06 22:18:03 +0900

    ユーザー管理機能修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :encrypted_password, :password, :password_confirmation,
+    validates :password, :password_confirmation,
               format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/, message: '英字と数字の両方（半角）を含めて設定してください。' }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '漢字・ひらがな・カタカナ（全角）で入力して下さい。' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '漢字・ひらがな・カタカナ（全角）で入力して下さい。' }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナ（全角）で入力して下さい。' }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナ（全角）で入力して下さい。' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '漢字・ひらがな・カタカナ（全角）で入力して下さい。' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナ（全角）で入力して下さい。' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
     validates :birthday
   end
 end
