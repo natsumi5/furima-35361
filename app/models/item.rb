8e67ab43503f86b1a5f9c0771f47b68f00c113f2
natsumi5 <ntm2038@gmail.com>
2021-07-10 15:14:25 +0900

    ActiveStorageとActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,23 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :status_id
-    validates :burden_id
-    validates :prefecture_id
-    validates :day_to_ship_id
-    validates :price
+      with_options numericality: { other_than: 1 } do
+        validates :category_id
+        validates :status_id
+        validates :burden_id
+        validates :prefecture_id
+        validates :day_to_ship_id
+      end
+        validates :price
   end
 
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :day_to_ship
 end
