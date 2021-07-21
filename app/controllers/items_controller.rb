645b7b3c7f929789bc24be6bd3f52f80554e0ec7
natsumi5 <ntm2038@gmail.com>
2021-07-21 14:35:10 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   before_action :redirect_to_index, only: [:edit, :update, :destroy]
